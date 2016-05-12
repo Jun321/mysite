b3aeff618fa750338e4c2b84a50f5fce00252438
Jun321 <q88096419@126.com>
2016-05-12 12:51:54 +0800

    rollback

--- walmart/urls.py
+++ walmart/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^accounts/login/$', views.index),
 	url(r'^logout/', views.logout_method, name='logout'),
 	url(r'^login/', views.login_method, name='login'),
 	url(r'^method/', views.method, name='method'),
