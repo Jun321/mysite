1cca347a6f950a73829bb82eb8569f356b7f1a23
Jun321 <q88096419@126.com>
2016-05-12 12:50:59 +0800

    login function implement and fix

--- walmart/urls.py
+++ walmart/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+	url(r'^accounts/login/$', views.index),
 	url(r'^logout/', views.logout_method, name='logout'),
 	url(r'^login/', views.login_method, name='login'),
 	url(r'^method/', views.method, name='method'),
